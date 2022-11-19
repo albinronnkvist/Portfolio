7b7e7788b5d5e0910795586381702fac24126db9
Albin <albinronnkvist@protonmail.com>
2022-11-19 14:40:36 +0100

    Update tech and text

--- components/sections/footer.js
+++ components/sections/footer.js
@@ -11,6 +11,7 @@ export default function Footer() {
           <a href="mailto:kontakt@albinronnkvist.se" target="_blank" rel="noreferrer" className="text-white" aria-label="Email"><i className="bi bi-envelope"></i></a>
           <a href="https://github.com/albinronnkvist" target="_blank" rel="noreferrer" className="text-white" aria-label="Github"><i className="bi bi-github"></i></a>
           <a href="https://stackoverflow.com/users/15634040/albinr" target="_blank" rel="noreferrer" className="text-white" aria-label="Stack Overflow"><i className="bi bi-stack-overflow"></i></a>
+          <a href="https://medium.com/@albinronnkvist" target="_blank" rel="noreferrer" className="text-white" aria-label="Medium"><i className="bi bi-medium"></i></a>
           <a href="https://www.linkedin.com/in/albin-r%C3%B6nnkvist-4b0aa0181/" target="_blank" rel="noreferrer" className="text-white" aria-label="LinkedIn"><i className="bi bi-linkedin"></i></a>
         </div>
       </div>
