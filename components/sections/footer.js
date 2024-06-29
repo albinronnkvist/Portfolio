ce49c8edea6ac75807e18188cc1cf13e74b397e1
Albin <albinronnkvist@protonmail.com>
2024-06-29 17:21:47 +0200

    Update stackoverflow link

--- components/sections/footer.js
+++ components/sections/footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
         <div className="flex flex-row gap-8 text-xl justify-center items-center mt-4">
           <a href="mailto:contact@albinronnkvist.me" target="_blank" rel="noreferrer" className="text-white" aria-label="Email"><i className="bi bi-envelope"></i></a>
           <a href="https://github.com/albinronnkvist" target="_blank" rel="noreferrer" className="text-white" aria-label="Github"><i className="bi bi-github"></i></a>
-          <a href="https://stackoverflow.com/users/15634040/albinr" target="_blank" rel="noreferrer" className="text-white" aria-label="Stack Overflow"><i className="bi bi-stack-overflow"></i></a>
+          <a href="https://stackoverflow.com/users/15634040/hackarmannen" target="_blank" rel="noreferrer" className="text-white" aria-label="Stack Overflow"><i className="bi bi-stack-overflow"></i></a>
           <a href="https://medium.com/@albinronnkvist" target="_blank" rel="noreferrer" className="text-white" aria-label="Medium"><i className="bi bi-medium"></i></a>
           <a href="https://www.linkedin.com/in/albin-r%C3%B6nnkvist-4b0aa0181/" target="_blank" rel="noreferrer" className="text-white" aria-label="LinkedIn"><i className="bi bi-linkedin"></i></a>
         </div>
